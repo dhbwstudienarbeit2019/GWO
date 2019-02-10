9f8cd01d8e247386e0386bec6503564a2556b283
laura-jane <laura.kaipl@online.de>
2019-02-10 13:23:12 +0100

    implemented first methods

--- src/wolf.ts
+++ src/wolf.ts
@@ -2,5 +2,21 @@ import { Position } from "./Point";
 
 export class Wolf {
     private position: Position;
+    private functionToOptimize: (x: number, y: number) => number;
 
+
+    public get FunctionToOptimize(): ((x: number, y: number) => number) {
+        return this.functionToOptimize;
+    }
+    public get Position(): Position {
+        return this.position;
+    }
+
+    public set Position(posiiton: Position) {
+        this.position = posiiton;
+    }
+
+    calculateFitness(): number {
+        return this.functionToOptimize(this.position.x, this.position.y);
+    }
 }
